d9b4fca4e1cab025496c44eb4d63949b4c4f750a
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-11-29 06:26:17 +0300

    Ver 1.0.0

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 // const bodyParser = require('body-parser'); deprecated
 
 const items = require('./routes/api/items');
@@ -21,6 +22,16 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
